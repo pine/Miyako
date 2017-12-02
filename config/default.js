ae3cbf59c44a11314935e318509612a89010316f
Pine Mizune <pinemz@gmail.com>
2017-12-02 22:01:54 +0900

    feat(project_mapping): add project <-> channel mapping

--- config/default.js
+++ config/default.js
@@ -1,13 +1,21 @@
 'use strict'
 
 module.exports = {
-  session: {
-    name: 'cloud.session.token',
-    value: 'XXX',
-  },
-
   jira: {
     baseUrl: 'https://example.atlassian.net/',
-    projectKeys: [],
+    session: {
+      name: 'cloud.session.token',
+      value: 'XXX',
+    },
   },
+
+  slack: {
+    token: 'XXX',
+    username: 'JIRA',
+    iconUrl: 'http://www.example.com/jira.png',
+  },
+
+  projects: [
+    { key: 'KEY', channel: 'general' },
+  ],
 }
